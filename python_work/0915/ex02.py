5d93fd540be9bae56cd430570ecbe361daa57961
dron512 <parkmyounghoi@gmail.com>
2023-09-15 16:15:40 +0900

    0915

--- python_work/0915/ex02.py
+++ python_work/0915/ex02.py
@@ -18,3 +18,28 @@ def doLoad():
         texts += ele.text
     web.close() #창 닫기
     return texts
+
+def doLoad2():
+    web = webdriver.Chrome() # 크롬 실행
+    web.get(url='https://dhlottery.co.kr/common.do?method=main') # 주소이동
+
+    main = web.window_handles
+    for i in main:
+        if i != main[0]:
+            web.switch_to.window(i)
+            web.close()
+
+    web.switch_to.window(main[0])
+    time.sleep(3)
+
+    no1 = web.find_element(By.ID,"drwtNo1").text
+    no2 = web.find_element(By.ID, "drwtNo2").text
+    no3 = web.find_element(By.ID, "drwtNo3").text
+    no4 = web.find_element(By.ID, "drwtNo4").text
+    no5 = web.find_element(By.ID, "drwtNo5").text
+    no6 = web.find_element(By.ID, "drwtNo6").text
+    bonus = web.find_element(By.ID, "bnusNo").text
+    
+    web.close() #창 닫기
+    return no1,no2,no3,no4,no5,no6,bonus
+
--- python_work/0915/friend.py
+++ python_work/0915/friend.py
@@ -12,11 +12,21 @@ class WindowClass(QMainWindow, form_class) :
         self.setupUi(self)
         self.savebtn.clicked.connect(self.saveEvent)
         self.loadbtn.clicked.connect(self.loadEvent)
+        self.loadbtn_2.clicked.connect(self.loadEvent2)
+
     def saveEvent(self):
         print('save')
+
     def loadEvent(self):
         result = ex02.doLoad()
         self.textid.setText(result)
+
+    def loadEvent2(self):
+        try:
+            result = ex02.doLoad2()
+            self.textpass.setText(str(result))
+        except Exception as e:
+            print(e)
 
 if __name__ == "__main__" :
     app = QApplication(sys.argv)
