3f520272eae77e0e4d3fdad10158a5ec6b0a1b53
dron512 <parkmyounghoi@gmail.com>
2023-10-12 11:30:36 +0900

    231012

--- spring_work/ex02/src/main/java/member/MemberDAO.java
+++ spring_work/ex02/src/main/java/member/MemberDAO.java
@@ -9,28 +9,9 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class MemberDAO {
-	@Autowired
-	private DataSource dataSource;
+	
 	@Autowired
 	private SqlSession sqlsession;
-	
-	public void test() {
-		System.out.println("data = "+dataSource);
-		Connection conn = null;
-		try {
-			conn = dataSource.getConnection();
-			System.out.println("연결됨");
-		}catch (Exception e) {
-			e.printStackTrace();
-		}finally {
-			try {
-				if(conn!=null)
-					conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-	}
 	
 	public void test2() {
 		System.out.println(sqlsession.selectList("member.selectAll"));
--- spring_work/ex02/src/main/java/member/MemberService.java
+++ spring_work/ex02/src/main/java/member/MemberService.java
@@ -2,6 +2,9 @@ package member;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import lombok.Getter;
+
+@Getter
 public class MemberService {
 	@Autowired
 	private MemberDAO memberDAO;
