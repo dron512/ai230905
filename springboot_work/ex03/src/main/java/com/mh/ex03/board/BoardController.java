3d0ec2c8f1b063853bcafc6c1562834d6f503d72
dron512 <parkmyounghoi@gmail.com>
2023-10-19 14:44:47 +0900

    231019

--- springboot_work/ex03/src/main/java/com/mh/ex03/board/BoardController.java
+++ springboot_work/ex03/src/main/java/com/mh/ex03/board/BoardController.java
@@ -4,7 +4,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -25,13 +27,41 @@ public class BoardController {
     }
 
     @GetMapping("writeform")
-    public String writeform(Model model) {
+    public String writeform(Model model,
+                            BoardReq boardReq,
+                            BindingResult result) {
         return "board/writeform";
     }
 
     @PostMapping("writeproc")
-    public String writeproc(@Valid BoardReq boardReq) {
+    public String writeproc(@Valid BoardReq boardReq, BindingResult result) {
+
+        if(result.hasErrors()){
+            System.out.println("에러 있음");
+            System.out.println(result);
+            return "board/writeform";
+        }
+        // 디비 저장
         System.out.println("boardReq " + boardReq);
-        return "board/writeform";
+        return "redirect:/board/list";
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
