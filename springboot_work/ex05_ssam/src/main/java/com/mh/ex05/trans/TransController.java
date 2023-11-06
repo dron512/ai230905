c3b55d83c7dd185ab8b539a2c0fd2be5901afa88
dron512 <parkmyounghoi@gmail.com>
2023-11-06 11:02:57 +0900

    231106

--- springboot_work/ex05_ssam/src/main/java/com/mh/ex05/trans/TransController.java
+++ springboot_work/ex05_ssam/src/main/java/com/mh/ex05/trans/TransController.java
@@ -1,15 +1,26 @@
 package com.mh.ex05.trans;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("trans")
 public class TransController {
 
+    @Autowired
+    TransService transService;
+
     @GetMapping("trans")
     public String trans(){
         return "trans/trans";
+    }
+
+    @PostMapping("req")
+    @ResponseBody
+    public String req(@RequestBody TranslationJson translationJson){
+        System.out.println(translationJson);
+        transService.main(translationJson.getText());
+        return "asdfasdfasedrqwer";
     }
 }
