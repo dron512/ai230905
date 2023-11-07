88a18f1b15c32e2d8187db4c25b623746c5bed8c
dron512 <parkmyounghoi@gmail.com>
2023-11-07 12:33:09 +0900

    231107

--- springboot_work/ex05_ssam/src/main/java/com/mh/ex05/member/AuthService.java
+++ springboot_work/ex05_ssam/src/main/java/com/mh/ex05/member/AuthService.java
@@ -1,16 +1,29 @@
 package com.mh.ex05.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService implements UserDetailsService {
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         System.out.println("일로온다");
-        System.out.println(username);
-        return null;
+        System.out.println(email);
+
+        return User.builder()
+                .username("aa@naver.com")
+                .password(passwordEncoder.encode("1234"))
+                .roles("ADMIN")
+                .build();
     }
 }
