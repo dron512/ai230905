481c50bc7ee1156aaa7624af62c3ad5d6616650b
dron512 <parkmyounghoi@gmail.com>
2023-11-20 11:44:30 +0900

    231120

--- springboot_work/ex05_ssam/src/main/java/com/mh/ex05/main/MainController.java
+++ springboot_work/ex05_ssam/src/main/java/com/mh/ex05/main/MainController.java
@@ -1,13 +1,25 @@
 package com.mh.ex05.main;
 
+import com.mh.ex05.member.Member;
+import com.mh.ex05.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
 
+    @Autowired
+    MemberRepository memberRepository;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        List<Member> list = memberRepository.findAll();
+        model.addAttribute("list",list);
+        System.out.println(list);
         return "index";
     }
 
